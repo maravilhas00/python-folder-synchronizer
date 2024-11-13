49d9fae9f28349329484da5272ada6e3d76bbd4f
maravilhas00 <nunomaravilhas2000@gmail.com>
2024-11-13 21:21:34 +0000

    Final version v1.1

--- folder_synchronizer/synchronizer.py
+++ folder_synchronizer/synchronizer.py
@@ -5,14 +5,14 @@ from datetime import datetime
 
 def log_entries(message, log_file_path):
     with open(log_file_path, "a") as log_file:
-        timestamp=datetime.now().strftime("%d/%m/%y %H:%M:%S")
+        timestamp=datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         log_file.write(f"[{timestamp}] {message}\n")
-        print(message)
+        print(f"[{timestamp}] {message}")
 
 def check_for_valid_folder(message):
     while True:
         folder_path=input(message)
-        if os.path.exists(folder_path):
+        if os.path.isdir(folder_path):
             return folder_path
         else:
             print("Please submit a valid folder path.")
@@ -48,7 +48,8 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
         #If it doesnt exist, creates one
         if not os.path.exists(replica_path):
             os.makedirs(replica_path)
-            log_entries(f"Created folder {replica_path}", log_file_path)
+            replica = replica_path.replace("/", "\\")
+            log_entries(f"Created folder {replica}", log_file_path)
 
         for file in files:
             source_file=os.path.join(root, file)
@@ -57,22 +58,25 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
             #If the file doesnt exist on the replica or the version on the source is recent, proceed to copy the file
             if (not os.path.exists(replica_file) or os.path.getmtime(source_file)>os.path.getmtime(replica_file)):
                 shutil.copy2(source_file, replica_file)
-                log_entries(f"Copied file ({file}) to {replica_path}", log_file_path)
+                replica = replica_path.replace("/", "\\")
+                log_entries(f"Copied file ({file}) to {replica}", log_file_path)
         
     #Delete files in replica that are not in the source
     for root, dirs, files in os.walk(replica_folder, topdown=False):
         #While running the files, it will pass the relative path of the file of the file to verify if it exists in the source and afterwards delete in replica if it doesnt
         relative_path=os.path.relpath(root, replica_folder)
         source_path=os.path.join(source_folder, relative_path)
-
+        
         #Verification if the file exists on the source
         for file in files:
             replica_file=os.path.join(root, file)
             source_file=os.path.join(source_path, file)
             if not os.path.exists(source_file):
                 #Delete file on replica
+                replica = root.replace("/", "\\")
+                log_entries(f"Deleted file ({file}) from {replica}", log_file_path)
                 os.remove(replica_file)
-                log_entries(f"Deleted file ({file}) from {root}", log_file_path)
+                
 
         #Verification if the folder exists on the source
         for dir in dirs:
@@ -80,8 +84,10 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
             source_sub_folder=os.path.join(source_path, dir)
             if not os.path.exists(source_sub_folder):
                 #Delete folder on replica
+                replica = replica_sub_folder.replace("/", "\\")
+                log_entries(f"Deleted folder {replica}", log_file_path)
                 shutil.rmtree(replica_sub_folder)
-                log_entries(f"Deleted folder {root}", log_file_path)
+                
 
 source_folder=check_for_valid_folder("Enter the Source folder path: ")
 replica_folder=check_for_valid_folder("Enter the Replica folder path: ")
