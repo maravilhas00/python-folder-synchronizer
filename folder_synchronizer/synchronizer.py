e48bce4d91ee8a3f30ed667de5003cd9ef94ae0c
maravilhas00 <nunomaravilhas2000@gmail.com>
2024-11-13 20:58:08 +0000

    First version v1.0

--- folder_synchronizer/synchronizer.py
+++ folder_synchronizer/synchronizer.py
@@ -3,12 +3,11 @@ import shutil
 import time
 from datetime import datetime
 
-
-def log_entries(message):
-    with open(log_file, "a") as log_file:
-        timestamp=datetime.now().strftime("%H:%M:%S %D-%m-%Y")
+def log_entries(message, log_file_path):
+    with open(log_file_path, "a") as log_file:
+        timestamp=datetime.now().strftime("%d/%m/%y %H:%M:%S")
         log_file.write(f"[{timestamp}] {message}\n")
-
+        print(message)
 
 def check_for_valid_folder(message):
     while True:
@@ -17,6 +16,26 @@ def check_for_valid_folder(message):
             return folder_path
         else:
             print("Please submit a valid folder path.")
+
+def check_for_log_file(message):
+    while True:
+        file_path=input(message)
+        if os.path.isfile(file_path):
+            return file_path
+        else:
+            print("Please submit a valid log file path.")
+
+def check_for_valid_number(message):
+    while True:
+        timer=input(message)
+        try:
+            refresh_timer=int(timer)
+            if refresh_timer>0:
+                return refresh_timer
+            else:
+                print("Insert a valid number.")
+        except ValueError:
+            print("Insert a valid input.")
 
 
 def folder_synchronizer(source_folder, replica_folder, log_file_path):
@@ -29,6 +48,7 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
         #If it doesnt exist, creates one
         if not os.path.exists(replica_path):
             os.makedirs(replica_path)
+            log_entries(f"Created folder {replica_path}", log_file_path)
 
         for file in files:
             source_file=os.path.join(root, file)
@@ -37,6 +57,7 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
             #If the file doesnt exist on the replica or the version on the source is recent, proceed to copy the file
             if (not os.path.exists(replica_file) or os.path.getmtime(source_file)>os.path.getmtime(replica_file)):
                 shutil.copy2(source_file, replica_file)
+                log_entries(f"Copied file ({file}) to {replica_path}", log_file_path)
         
     #Delete files in replica that are not in the source
     for root, dirs, files in os.walk(replica_folder, topdown=False):
@@ -51,6 +72,7 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
             if not os.path.exists(source_file):
                 #Delete file on replica
                 os.remove(replica_file)
+                log_entries(f"Deleted file ({file}) from {root}", log_file_path)
 
         #Verification if the folder exists on the source
         for dir in dirs:
@@ -59,23 +81,13 @@ def folder_synchronizer(source_folder, replica_folder, log_file_path):
             if not os.path.exists(source_sub_folder):
                 #Delete folder on replica
                 shutil.rmtree(replica_sub_folder)
-
+                log_entries(f"Deleted folder {root}", log_file_path)
 
 source_folder=check_for_valid_folder("Enter the Source folder path: ")
 replica_folder=check_for_valid_folder("Enter the Replica folder path: ")
-log_file_path=check_for_valid_folder("Enter the path for the log file: ")
-refresh_timer=int(input("Refresh timer in seconds: "))
-
-
-
-
-#replica_folder="C:/xampp/htdocs/Replica"
-#log_file_path="C:/xampp/htdocs/Logs"
-#source_folder="C:/xampp/htdocs/Source"
-
-
+log_file_path=check_for_log_file("Enter the log file path: ")
+refresh_timer=check_for_valid_number("Refresh timer in seconds: ")
 
 while True:
     folder_synchronizer(source_folder, replica_folder, log_file_path)
-    print(f"Synchronization complete. Waiting for {refresh_timer} seconds...")
     time.sleep(refresh_timer)
